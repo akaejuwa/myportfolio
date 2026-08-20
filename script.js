d6ca32ac631ce28993b5c47a9a7142787485a125
akaejuwa <105069286+akaejuwa@users.noreply.github.com>
2026-08-20 17:38:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const topBtn = 
+document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 200) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
